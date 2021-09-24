32cf7e2cd0c36d341259f9d0b1876f1789e5afb7
ivulovic <ivn.vulovic@gmail.com>
2021-09-24 20:24:40 +0200

    Added Redux Cache Example

--- src/components/Browser/index.js
+++ src/components/Browser/index.js
@@ -2,7 +2,7 @@ import { useContext } from "react";
 import FolderVisibilityContext from "providers/FolderVisibility/FolderVisibilityContext";
 import "./style.scss";
 
-export default function Browser({children, onClose, title}){
+export default function Browser({children, onClose, title, contentHeight}){
   const {lastOpened, setLastOpened} = useContext(FolderVisibilityContext);
   const isCurrentlyOpened = lastOpened === title;
   const handleSelection = () => {
@@ -10,19 +10,21 @@ export default function Browser({children, onClose, title}){
       setLastOpened(title);
     }
   }
-  return <div onClick={handleSelection}  className={`browser-wrap ${isCurrentlyOpened ? 'in-focus' : ''}`}>
-          <div className="browser ui-droppable" style={{display:'block'}}>
-            <div className="browser-panel">
-              <div className="browser-close" onClick={()=>onClose()}></div> 
-              <div className="browser-title">{title}</div>
-            </div>
-            <div className="website">
-              <div className="website-menu">
-                <div className="website-inner">
-                  {children}
-                </div>
-              </div>
-            </div>
+  return <div onClick={handleSelection}  className={`browser-wrap ${isCurrentlyOpened ? 'in-focus' : ''}`} style={{
+    transform: `translate(-50%, -${contentHeight}px)`,
+  }}>
+    <div className="browser ui-droppable" style={{display:'block'}}>
+      <div className="browser-panel">
+        <div className="browser-close" onClick={()=>onClose()}></div> 
+        <div className="browser-title">{title}</div>
+      </div>
+      <div className="website">
+        <div className="website-menu">
+          <div className="website-inner">
+            {children}
           </div>
         </div>
+      </div>
+    </div>
+  </div>
 }
--- src/containers/Desktop/index.js
+++ src/containers/Desktop/index.js
@@ -1,4 +1,4 @@
-import { useState,useContext } from 'react';
+import { useState,useContext, useRef, useMemo } from 'react';
 import Icon from 'components/Icon';
 import webpageImg from 'images/webpage7.png';
 import folderImg from 'images/folder.png';
@@ -10,14 +10,16 @@ import Browser from 'components/Browser';
 const defaultQueryParams = 'fontsize=14&hidenavigation=1&theme=light&view=preview&autoresize=1&moduleview=0';
 
 const webpages = [
-  {title: 'Class Component Lifecycle', url:`https://codesandbox.io/embed/react-grid-layout-react-lifecycle-rhk5y?${defaultQueryParams}`},
-  {title: 'Keys and List', url:`https://codesandbox.io/embed/react-list-and-keys-f4xzd?${defaultQueryParams}`},
-  {title: 'Clojure Increment', url:`https://codesandbox.io/embed/react-clojure-task-dgvvy?${defaultQueryParams}`},
-  {title: 'Reconciliation', url:`https://codesandbox.io/embed/react-reconciliation-kwzfx?${defaultQueryParams}`}
+  {title: 'Class Component Lifecycle', url: `https://codesandbox.io/embed/react-grid-layout-react-lifecycle-rhk5y?${defaultQueryParams}`},
+  {title: 'Keys and List', url: `https://codesandbox.io/embed/react-list-and-keys-f4xzd?${defaultQueryParams}`},
+  {title: 'Clojure Increment', url: `https://codesandbox.io/embed/react-clojure-task-dgvvy?${defaultQueryParams}`},
+  {title: 'Reconciliation', url: `https://codesandbox.io/embed/react-reconciliation-kwzfx?${defaultQueryParams}`},
+  {title: 'Cache Concept', url: `https://codesandbox.io/embed/react-redux-cache-2tui4?${defaultQueryParams}`}
 ]
 
 export default function Desktop(){
   
+  const contentRef = useRef();
   const [page, setPage] = useState({});
   
   const {setLastOpened} = useContext(FolderVisibilityContext);
@@ -35,10 +37,13 @@ export default function Desktop(){
         {icon: webpageImg, title: "Class Component Lifecycle", onClick: () => openPage("Class Component Lifecycle")},
         {icon: webpageImg, title: "Clojure Increment", onClick: () => openPage("Clojure Increment")},
     ]},
+    {title: 'React Redux', content: [
+      {icon: webpageImg, title: "Cache Concept", onClick: () => openPage("Cache Concept") }
+    ]},
     {title: 'React Basics', content: [
       {icon: webpageImg, title: "Keys and List", onClick: () => openPage("Keys and List")},
       {icon: webpageImg, title: "Reconciliation", onClick: () => openPage("Reconciliation")},
-    ]}
+    ]},
   ]
   const [folders, setFolders] = useState(folderList.map(x => ({...x, isOpened: false})));
 
@@ -53,8 +58,9 @@ export default function Desktop(){
   const closeBrowser = () => {
     setPage({});
   }
+  const contentHeight = useMemo(()=> contentRef.current ? contentRef.current.getBoundingClientRect().height : 0, [contentRef.current])
   return <div className="desktop">
-          <div className="column-list">
+          <div className="column-list" ref={contentRef}>
             {folders.map(x=> {
               return <div key={x.title} style={{position:'relative'}}>
                       <Icon icon={folderImg} title={x.title} onClick={openFolder}/>
@@ -62,7 +68,7 @@ export default function Desktop(){
                     </div>
             })}
           </div>
-          {page.url && <Browser onClose={closeBrowser} title={page.title}>
+          {page.url && <Browser onClose={closeBrowser} title={page.title} contentHeight={contentHeight}>
           <iframe className="iframe"
           src={page.url}></iframe>
           </Browser>}
